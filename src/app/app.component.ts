b9d470e58fea5c06fcfb0ca1d0729ac29381e84c
Dimitri Walters <me@dimi.io>
2024-09-05 13:43:10 -0400

    Use sizer component in the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SizerComponent } from './sizer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SizerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-angular-app';
+  fontSizePx = 16;
 }
